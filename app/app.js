cf37251aec30a8ebc6440180f220c6350ec86e9a
Sharad Shinde <shindesharad71@gmail.com>
2016-12-16 11:19:24 +0530

    data in object form added

--- app/app.js
+++ app/app.js
@@ -2,4 +2,9 @@ var app = angular.module("crudapp", []);
 
 app.controller('AppController', function() {
 
+    this.dataArray = [{ id: 1599, name: "ABC", email: "A@b.com", password: "qwerfdff" },
+        { id: 1599, name: "ABC", email: "A@b.com", password: "qwerfdff" },
+        { id: 1599, name: "ABC", email: "A@b.com", password: "qwerfdff" },
+        { id: 1599, name: "ABC", email: "A@b.com", password: "qwerfdff" }
+    ];
 });
