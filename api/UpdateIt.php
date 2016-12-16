f562a8e29ed1c47a5f647fcb0de41ef988926446
Sharad Shinde <shindesharad71@gmail.com>
2016-12-16 16:43:10 +0530

    read opration test done

---
+++ api/UpdateIt.php
@@ -0,0 +1,20 @@
+<?php
+
+	$_POST = json_decode(file_get_contents('php://input'), true);
+
+	global $con;
+
+    $hostname = 'localhost';    // Host Name
+    
+    $user = 'root';             // username of host
+    
+    $password = '';             // password of host
+    
+    $dbname = 'angular';            //database name
+            
+    $con = new mysqli($hostname,$user,$password,$dbname);
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        die();
+    }
